c100986234754189c6d22ee99d67f59126bd645c
Badzagh <saba789badzaghua@gmail.com>
2022-03-23 21:39:00 +0400

    add delete/restore

--- src/App.js
+++ src/App.js
@@ -2,85 +2,27 @@ import './App.css';
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
 import Typography from '@mui/material/Typography';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import { Container, TextField, FormControlLabel, Box, Input } from '@mui/material';
-
-let activePageNum = 1;
-let maxPageNum = 1;
+import {  Box } from '@mui/material';
+import {makeRequestChange, makeRequestJokeList, makeRequestRandomJoke} from './DataFetching.js';
 
 function App() {
 
-  const [items, setItems] = useState([]);
-  const [itemName, setItemName] = useState("");
-  const [editItemName, setEditItemName] = useState("")
-  const [visibilityEditInputContainer, setVisibilityEditInputContainer] = useState("input-edit-container-hidden");
-  const [pageCount, setPageCount] = useState([]);
-  const [itemId, setItemId] = useState("");
-  const [page, setPage] = useState(1)
-  const [taskComplete, setTaskComplete] = useState([false, false, false, false, false])
-  const [txtEditButton, setTxtEditButton] = useState("Edit")
-  const [txtDeleteButton, setTxtDeleteButton] = useState("Delete")
-
-  //////////
   const [randomJoke, setRandomJoke] = useState([]);
   const [jokeList, setJokeList] = useState([]);
   const [showError, setShowError] = useState(false);
-
-  //make get request
-  const makeRequest = (Page) => {
-    axios({
-      method: 'get',
-      url: `https://new-website-todo.herokuapp.com/items/choose?page=${Page}`,
-      headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      .then((response) => {
-
-        response.data.Info.forEach((item, index) => {
-
-          if(item.checkTask === "complete"){
-            taskComplete[index] = true
-          } else {
-            taskComplete[index] = false
-          }
-          setTaskComplete(taskComplete)
-          
-        })
-        setItems(response.data.Info);
-      })
-  } 
+  let error = false;
 
   //
   useEffect(() => {
     //list jokes
     setTimeout(() => {
-      axios({
-        method: 'get',
-        url: 'http://localhost:8000/items',
-        headers: {
-            "Content-Type": "application/json"
-        }
-      })
-      .then((response) => {
-        console.log(response.data)
-        setJokeList(response.data)
-      }).then(() => {
-        //makeRequest(1)
-      })
+      makeRequestJokeList({setJokeList}, )
     }, 10)
-  }, [])
-
-  // 
+  }, [jokeList])
 
   const handleClickAdd = (e) => {
-  
-    //e.preventDefault();
-    console.log(randomJoke.value)
-    console.log(randomJoke.created_at)
+    //check equal values
     axios({
       method: 'get',
       url: 'http://localhost:8000/items',
@@ -91,134 +33,126 @@ function App() {
     .then((response) => {
       console.log(response.data)
       setJokeList(response.data)
+
       response.data.forEach((item) => {
-        console.log(item)
-        
         if(item.value === randomJoke.value){
-          console.log(item.value)
-          console.log(randomJoke.value)
-          console.log("equal values")
           setShowError(true)
-        } else {
-          setShowError(false)
-        }
+          error = true
+        } 
       })
+
+      if(randomJoke.value === undefined){
+        setShowError(true)
+        error = true
+      } 
+
     }).then(() => {
-      //
-      axios({
-        method: 'post',
-        url: 'http://localhost:8000/items/add',
-        responseType: 'json',
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json"
-        },
-        data : {
-            value: randomJoke.value,
-            created_at: randomJoke.created_at
-        }
-      })
-      .then((response) =>  {
-        console.log(response)
-      })
-      .then((response) =>  {
-        setRandomJoke([])
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      //////add joke
+      if(!error){
+        axios({
+          method: 'post',
+          url: 'http://localhost:8000/items/add',
+          responseType: 'json',
+          headers: {
+              'Accept': 'application/json',
+              "Content-Type": "application/json"
+          },
+          data : {
+              value: randomJoke.value,
+              created_at: randomJoke.created_at,
+              updated_at: randomJoke.updated_at,
+              is_deleted: "Delete",
+              opacity: "1",
+              text_decoration: "none"
+          }
+        })
+        .then((response) =>  {
+          console.log(response)
+        })
+        .then((response) =>  {
+          setRandomJoke([])
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      }  
     })
   }
 
-  const handleClickGanarate = (e) => {
+  const handleClickGenerate = (e) => {
   
     e.preventDefault();
-
-    ///////ganarate random joke
-    axios({
-      method: 'get',
-      url: 'https://api.chucknorris.io/jokes/random',
-      headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      .then((response) => {
-        setRandomJoke(response.data);
-        console.log(response.data)
-      })
+    //ganarate random joke
+    makeRequestRandomJoke({setRandomJoke})
   }
 
-  //Delete/Cancel
-  const handleClickDeleteCancel = (e) => {
+  //Delete/Restore
+  const handleClickDeleteRestore = (e) => {
     
-    console.log(e.target.value)
-    setTxtDeleteButton("Restore");
-
-    if(e.target.name === "Delete from data"){
-      axios.delete(`http://localhost:8000/items/delete/${e.target.id}`, {
-        body: e.target.id
-      })
-      .then(() => {
-        setTxtDeleteButton("Restore");
-        window.location.reload();
-      })
-    } else {
-    
-      setVisibilityEditInputContainer("input-edit-container-hidden")
-      setTxtEditButton("Edit");
-      //setTxtDeleteButton("Delete");
+    if(e.target.name === "Delete"){
+      makeRequestChange("Restore","0.3","line-through", e.target.id)
     }
-    
+    if(e.target.name === "Restore"){
+      makeRequestChange("Delete","1","none", e.target.id)
+    }
   }
 
-  
   //
 
   return (
-    <div>
-        <Box  sx={{ maxWidth: '45%',
+    <div id="app">
+        <Box  sx={{ maxWidth: '98%',
                     display: 'flex',
-                    flexWrap: 'wrap',
-                    justifyContent: 'center',
-                    marginLeft: "auto",
+                    flexDirection: 'column',
+                    justifyContent: 'start',
+                    marginLeft: "10px",
                     marginRight: "auto",
-                    paddingTop: "150px"
+                    paddingTop: "100px"
                   }}> 
-            <Box minWidth={"297px"} className="input-add-container" >                                   
-              <p>{randomJoke.value}</p>
-              <span>{randomJoke.created_at}</span>
-              <img src={randomJoke.icon_url}></img>
-              <Button size="medium" onClick={handleClickAdd} id="input-button" type="submit">ADD</Button>
-              <Button size="medium" onClick={handleClickGanarate} id="input-button" type="submit">Ganarate</Button>
+            <Box sx={{ maxWidth: '80%',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    marginBottom: "10px"
+                  }}>
+              <Button size="medium" onClick={handleClickAdd} id="button-add" type="submit">ADD</Button>
+              <Button size="medium" onClick={handleClickGenerate} id="button-generate">Generate</Button>
+            </Box>
+            <Box minWidth={"297px"} className="container" >                                   
+              <Typography>{randomJoke.value}</Typography>
+              <Typography variant="caption">{randomJoke.created_at}</Typography>
             </Box>  
             {showError &&
-                  <span>try other joke</span>
+                  <span style={{color: "red"}}>try other joke</span>
             }
         </Box>
-        
-            {jokeList.map((item, index) => (
-              <Box 
-                key={item._id} 
-                sx={{   
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: 'space-between',
-                  height: "25px",
-                  paddingBottom: "8.5px",
-                  paddingTop: "8.5px"
-                }}>
-                <p>{item.value}</p>
-                <span>{item.created_at}</span>
-                <Box>
-                  <Button onClick={handleClickDeleteCancel} id={item._id} className="delete-btn" name={txtDeleteButton} onChange={(e) => {}}>{txtDeleteButton}</Button>
-                </Box>
-              </Box>
-            ))}
-            
-        
+        {jokeList.map((item, index) => (
+          <Box 
+            key={item._id} 
+            sx={{   
+              display: "flex",
+              alignItems: "center",
+              justifyContent: 'space-between',
+              height: "125px",
+              paddingBottom: "8.5px",
+              paddingTop: "8.5px",
+              marginBottom: "10px",
+              marginLeft: "10px"
+            }}>
+            <Box sx={{   
+                      minWidth: '280px',
+                      width: '80%'
+                    }}>
+              <Typography style={{opacity: item.opacity, textDecoration: item.text_decoration}}>{item.value}</Typography>
+              <Typography variant="caption" style={{opacity: item.opacity, textDecoration: item.text_decoration}}>{item.created_at}</Typography>
+            </Box>
+            <Box>
+              <Button  onClick={handleClickDeleteRestore} id={item._id} className="delete-btn" value={item.updated_at} name={item.is_deleted}>{item.is_deleted}</Button>
+            </Box>
+          </Box>
+        ))} 
     </div>
   );
 }
 
-
+///
 export default App;
---
+++ src/DataFetching.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { useState } from "react";
+
+export const makeRequestChange = (isDeleted,opacity,textDecoration, id) => {
+    axios({
+        method: 'put',
+        url: `http://localhost:8000/items/change/${id}`,
+        responseType: 'json',
+        data : {
+            is_deleted: isDeleted,
+            opacity: opacity,
+            text_decoration: textDecoration
+        },
+        })
+        .catch((error) => {
+        console.log(error);
+        });
+}
+export const makeRequestJokeList = ({setJokeList}) => {
+    axios({
+        method: 'get',
+        url: 'http://localhost:8000/items',
+        headers: {
+            "Content-Type": "application/json"
+        }
+      })
+      .then((response) => {
+        setJokeList(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+}
+export const makeRequestRandomJoke = ({setRandomJoke}) => {
+    axios({
+        method: 'get',
+        url: 'https://api.chucknorris.io/jokes/random',
+        headers: {
+            "Content-Type": "application/json"
+        }
+      })
+      .then((response) => {
+        setRandomJoke(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+}
